a214860461b4edf04932a0517aaea6d9fabfc9b4
stendro <33956801+stendro@users.noreply.github.com>
2017-12-30 14:58:03 +0100

    Revert "qpnp-smbcharger: Publish charger voltage to the usb psy"
    useless for LG kernel
    
    This reverts commit b60ddb178bde464f4939158d53f583a389514d4d.

--- drivers/power/qpnp-smbcharger_alice.c
+++ drivers/power/qpnp-smbcharger_alice.c
@@ -2851,8 +2851,6 @@ static void smbchg_parallel_usb_enable(struct smbchg_chip *chip,
 	}
 #endif
 
-	power_supply_set_voltage_limit(chip->usb_psy,
-			(chip->vfloat_mv + 50) * 1000);
 	/* Set USB ICL */
 #ifdef CONFIG_LGE_PM_PARALLEL_CHARGING
 	target_icl_ma = get_effective_result_locked(chip->usb_icl_votable);
@@ -3863,11 +3861,8 @@ static int smbchg_float_voltage_set(struct smbchg_chip *chip, int vfloat_mv)
 
 	if (rc)
 		dev_err(chip->dev, "Couldn't set float voltage rc = %d\n", rc);
-	else {
+	else
 		chip->vfloat_mv = vfloat_mv;
-		power_supply_set_voltage_limit(chip->usb_psy,
-				chip->vfloat_mv * 1000);
-	}
 
 	return rc;
 }
